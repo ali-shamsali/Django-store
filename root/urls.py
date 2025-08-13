efd3ebadf10579459b41163fec841313fae694b5
ali-shamsali <alishamsali2079@gmail.com>
2025-08-13 19:04:12 +0330

    add template faq

--- root/urls.py
+++ root/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.shortcuts import render
-from .views import home , contact , about
+from .views import home , contact , about , faq
 
 app_name = 'root'
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', home , name='home'),
     path('contact', contact , name='contact'),
     path('about', about , name='about'),
+    path('faq', faq , name='faq'),
 ]
