ac84f4910482357cf64656e65edb542fdefade1f
ali-shamsali <alishamsali2079@gmail.com>
2025-08-22 17:25:59 +0330

    Implement full login system: authentication, login view, template form, error messages, and dynamic header

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from django.shortcuts import render
-from .views import views_login
+from .views import views_login , views_logout
 app_name = 'accounts'
 
 urlpatterns = [
     path('login', views_login , name='login'),
+    path('logout/', views_logout , name='logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from django.contrib.auth import authenticate, login , logout
+from django.contrib import messages
 
 # Create your views here.
 
 def views_login(request):
+    if request.user.is_authenticated:
+        return redirect('root:home') 
+    
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('root:home')  
+        else:
+            messages.error(request, "نام کاربری یا رمز عبور اشتباه است")
     return render(request,'accounts/login.html')
+
+def views_logout(request):
+    logout(request)
+    return redirect('accounts:login')
