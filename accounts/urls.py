71049d6deed713a6d703106c2bf811781c3fb1bb
ali-shamsali <alishamsali2079@gmail.com>
2025-08-24 12:22:28 +0330

    Add user signup view with UserCreationForm

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from django.shortcuts import render
-from .views import views_login , views_logout
+from .views import views_login , views_logout , views_signup
 app_name = 'accounts'
 
 urlpatterns = [
     path('login', views_login , name='login'),
+    path('signup', views_signup , name='signup'),
     path('logout/', views_logout , name='logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render , redirect
 from django.contrib.auth import authenticate, login , logout
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
@@ -20,6 +21,24 @@ def views_login(request):
             messages.error(request, "نام کاربری یا رمز عبور اشتباه است")
     return render(request,'accounts/login.html')
 
+def views_signup(request):
+    if request.user.is_authenticated:
+        return redirect('root:home')
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()   
+            login(request, user) 
+            messages.success(request, "ثبت‌نام با موفقیت انجام شد 🎉")
+            return redirect('root:home')
+        else:
+            messages.error(request, "خطایی در ثبت‌نام وجود دارد")
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/signup.html', {'form': form})
+
 def views_logout(request):
-    logout(request)
+    if request.user.is_authenticated:
+        logout(request)
     return redirect('accounts:login')
